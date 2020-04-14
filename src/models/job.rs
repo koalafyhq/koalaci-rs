3e0f32f1738b019b3056b14942d32224e4b30ed3
Fariz Rizaldy <farizrizaldy@icloud.com>
2020-04-14 17:01:58 +0700

    feat: create functionality for handle job creation

--- src/models/job.rs
+++ src/models/job.rs
@@ -1,11 +1,24 @@
 extern crate redis;
 
 use crate::models::Db;
+
 use redis::Commands;
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone)]
 pub struct Job {
   db: Db,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Deployment {
+  pub deployment_id: String,
+  pub project_id: String,
+  pub project_branch: String,
+  pub project_repo_url: String,
+  pub project_build_command: String,
+  pub project_package_manager: String,
+  pub project_dist_directory: String,
 }
 
 impl Job {
@@ -13,14 +26,28 @@ impl Job {
     Job { db: db }
   }
 
-  // TODO: create this
-  pub fn create(&self, user_id: String) -> String {
-    let job = format!("deployment:{}", user_id);
+  pub fn is_current_job_running(&self, user_id: &String, job_id: &String) -> bool {
+    let job = format!("deployment:{}:{}", user_id, job_id);
 
     let mut db = self.db.lock().unwrap();
     let result: String = db.get(job).unwrap_or("".to_string());
 
-    result
+    result != "" && result == "BUILDING"
+  }
+
+  pub fn update_job_status(&self, user_id: &String, payload: &Deployment, status: String) {
+    let job = format!("deployment:{}:{}", user_id, payload.deployment_id);
+
+    let mut db = self.db.lock().unwrap();
+    let _: () = db.set(job, status).unwrap();
+  }
+
+  pub fn write_log(&self, user_id: &String, payload: &Deployment, line: String) {
+    let log = format!("log:{}:{}", user_id, payload.deployment_id);
+    let line = format!("{} \n", line);
+
+    let mut db = self.db.lock().unwrap();
+    let _: () = db.append(log, line).unwrap();
   }
 
   // TODO: Can we store `user_id` on context?
